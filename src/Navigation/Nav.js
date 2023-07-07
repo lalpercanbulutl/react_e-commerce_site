63ab5c5a2868d9874d366e758d3daeb4f8eed824
Alper Can Bulut <77748092+lalpercanbulutl@users.noreply.github.com>
2023-07-07 17:18:24 +0300

    nav and index css

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,8 +1,27 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import "./Nav.css";
 
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <div className="nav-conainer">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your search"
+        />
+      </div>
+      <a href="#">
+        <FiHeart className="nav-icons" />
+      </a>
+      <a href="#">
+        <AiOutlineShoppingCart className="nav-icons" />
+      </a>
+      <a href="#">
+        <AiOutlineUserAdd className="nav-icons" />
+      </a>
+    </nav>
+  );
 }
 export default Nav;
